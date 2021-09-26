450db5c7e8636e6bcfd74acfd2e61680c22e52d1
sandi-hermawan <sandi.tugas01@gmail.com>
2021-09-26 08:51:33 +0700

    tugas keenam

---
+++ Match Three/Assets/Script/FlowManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowManager : MonoBehaviour
+{
+    #region Singleton
+
+    private static FlowManager _instance = null;
+
+    public static FlowManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<FlowManager>();
+
+                if (_instance == null)
+                {
+                    Debug.LogError("Fatal Error: GameFlowManager not Found");
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    #endregion
+
+    public bool IsGameOver { get { return isGameOver; } }
+
+    private bool isGameOver = false;
+
+
+    [Header("UI")]
+    public UIGameOver GameOverUI;
+
+    void Start()
+    {
+        isGameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameOver()
+    {
+        isGameOver = true;
+        ScoreManager.Instance.SetHighScore();
+        GameOverUI.Show();
+    }
+}
---
+++ Match Three/Assets/Script/TimeManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    #region Singleton
+
+    private static TimeManager _instance = null;
+
+    public static TimeManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<TimeManager>();
+
+                if (_instance == null)
+                {
+                    Debug.LogError("Fatal Error: TimeManager not Found");
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    #endregion
+
+    public int duration;
+
+    private float time;
+
+    void Start()
+    {
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (FlowManager.Instance.IsGameOver)
+        {
+            return;
+        }
+
+        if (time > duration)
+        {
+            FlowManager.Instance.GameOver();
+            return;
+        }
+
+        time += Time.deltaTime;
+    }
+    public float GetRemainingTime()
+    {
+        return duration - time;
+    }
+}
